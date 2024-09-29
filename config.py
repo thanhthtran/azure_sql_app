dbc9c5d83aa38b73b625beedd03148247827f10f
thanhthtran <liamthtr@gmail.com>
2024-09-29 10:01:15 -0700

    add all files

---
+++ config.py
@@ -0,0 +1,60 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config(object):
+    
+    # basedir = os.path.abspath(os.path.dirname(__file__))
+    # dotenv_path = os.path.join(basedir, '.env')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') 
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') 
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER')
+
+    SQL_SERVER = os.environ.get('SQL_SERVER') 
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') 
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') 
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') 
+    print(SQL_USER_NAME)
+    
+    # print(f"SQL_SERVER: {SQL_SERVER}")
+    # print(f"SQL_DATABASE: {SQL_DATABASE}")
+    # print(f"SQL_USER_NAME: {SQL_USER_NAME}")
+    # print(f"SQL_PASSWORD: {SQL_PASSWORD}")
+    if SQL_SERVER and SQL_DATABASE and SQL_USER_NAME and SQL_PASSWORD:
+        SQLALCHEMY_DATABASE_URI = f'mssql+pyodbc://{SQL_USER_NAME}@{SQL_SERVER}:{SQL_PASSWORD}@{SQL_SERVER}:1433/{SQL_DATABASE}?driver=ODBC+Driver+17+for+SQL+Server'
+        # print(SQLALCHEMY_DATABASE_URI)
+    else:
+        print("Error: One or more required SQL environment variables are missing.")
+        SQLALCHEMY_DATABASE_URI = ""
+    # Below URI may need some adjustments for driver version, based on your OS, if running locally
+    # SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    ### Info for MS Authentication ###
+    ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET_VALUE')
+    # In your production app, Microsoft recommends you to use other ways to store your secret,
+    # such as KeyVault, or environment variable as described in Flask's documentation here:
+    # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
+    # CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+    # if not CLIENT_SECRET:
+    #     raise ValueError("Need to define CLIENT_SECRET environment variable")
+
+    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
+    # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
+
+    CLIENT_ID = os.environ.get('CLIENT_SECRET_ID')
+
+    REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
+
+    # You can find the proper permission names from this document
+    # https://docs.microsoft.com/en-us/graph/permissions-reference
+    SCOPE = ["User.Read"] # Only need to read user profile for this app
+
+    SESSION_TYPE = "filesystem"  # Token cache will be stored in server-side session
+    
+    
+    
+my_config = Config()
